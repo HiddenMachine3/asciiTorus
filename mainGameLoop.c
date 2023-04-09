443b240d0d1b5dd730d640d2fff8ec3658ac6193
hidden_machine(Suhas) <suhashmsocial@gmail.com>
2023-04-09 20:30:12 +0530

    finished animation

--- mainGameLoop.c
+++ mainGameLoop.c
@@ -11,8 +11,8 @@
 #define h 50
 #define circle_radius 10
 #define torus_radius 10
-#define num_circle_points 10
-#define num_torus_rings 10
+#define num_circle_points 20
+#define num_torus_rings 20
 const int num_torus_points = (num_circle_points * num_torus_rings);
 
 char screen[w][h];
@@ -25,7 +25,11 @@ void delay_millis(int milli_seconds);
 #define frames 1000
 
 /**** Math members ****/
+gsl_matrix *YRotationMatrix, *XRotationMatrix, *ZRotationMatrix;
+
+void update_rotation_matrix_x(gsl_matrix *rot_matrix, double angle);
 void update_rotation_matrix_y(gsl_matrix *rot_matrix, double angle);
+void update_rotation_matrix_z(gsl_matrix *rot_matrix, double angle);
 
 /**
  gcc -Wall main.c -lgsl -lgslcblas -o "main.exe"
@@ -52,6 +56,13 @@ void init()
             screen[x][y] = ' ';
     screen[0][0] = screen[w - 1][0] = screen[0][h - 1] = screen[w - 1][h - 1] = '#';
 
+    // init rotation matrices
+    YRotationMatrix = gsl_matrix_alloc(3, 3);
+    XRotationMatrix = gsl_matrix_alloc(3, 3);
+    ZRotationMatrix = gsl_matrix_alloc(3, 3);
+    gsl_matrix_set_identity(XRotationMatrix);
+    gsl_matrix_set_identity(YRotationMatrix);
+    gsl_matrix_set_identity(ZRotationMatrix);
     /*****init torus******/
 
     // make circle
@@ -73,42 +84,36 @@ void init()
     for (i = 0; i < num_circle_points; gsl_vector_add(circlepoints[i], first_ring_midpoint), i++)
         ;
 
-    gsl_matrix *rotation_matrix = gsl_matrix_alloc(3, 3);
-
     double ring_angle_inc = 2 * M_PI / num_torus_rings;
     for (int ring_number = 0; ring_number < num_torus_rings; ring_number++)
     {
-        update_rotation_matrix_y(rotation_matrix, ring_number * ring_angle_inc);
+        update_rotation_matrix_y(YRotationMatrix, ring_number * ring_angle_inc);
 
         for (i = 0; i < num_circle_points; i++) // we multiple each position vector of the circle by the rotation matrix, to "rotate" it about the origin
         {
             toruspoints[(ring_number * num_circle_points) + i] = gsl_vector_alloc(3);
             gsl_blas_dgemv(CblasNoTrans,                                        // no transpose
                            1.0,                                                 // alpha=1
-                           rotation_matrix,                                     // matrix
+                           YRotationMatrix,                                     // matrix
                            circlepoints[i],                                     // vector
                            0.0,                                                 // beta,
                            toruspoints[(ring_number * num_circle_points) + i]); // result
         }
     }
 
-    gsl_vector *torus_mid_point = gsl_vector_alloc(3);
-    memcpy(torus_mid_point->data, (double[]){w/2 , h/2 , 0}, sizeof(double) * 3);
-    for (i = 0; i < num_torus_points; gsl_vector_add(toruspoints[i], torus_mid_point), i++)
-        ;
+    // gsl_vector *torus_mid_point = gsl_vector_alloc(3);
+    // memcpy(torus_mid_point->data, (double[]){w / 2, h / 2, 0}, sizeof(double) * 3);
+    // for (i = 0; i < num_torus_points; gsl_vector_add(toruspoints[i], torus_mid_point), i++)
+    //     ;
+    // gsl_vector_free(torus_mid_point);
 
-    // print the torus on the screen
-    for (int i = 0, x, y; i < num_torus_points; i++)
-    {
-        x = (gsl_vector_get(toruspoints[i], 0));
-        y = (gsl_vector_get(toruspoints[i], 1));
-        if (x >= 0 && y >= 0 && x < w && y < h)
-            screen[x][y] = '#';
-    }
-
-    gsl_vector_free(torus_mid_point);
     gsl_vector_free(first_ring_midpoint);
-    gsl_matrix_free(rotation_matrix);
+
+    // setting up spinning
+    update_rotation_matrix_x(XRotationMatrix, 2 * M_PI / 1500);
+    update_rotation_matrix_y(YRotationMatrix, 0);
+    update_rotation_matrix_z(ZRotationMatrix,  2 * M_PI / 1500);
+
     //  // where we want the middle of the torus
     //  torus_mid_point = gsl_vector_alloc(3);
     //  memcpy(torus_mid_point->data, (double[]){w / 2, h / 2, 0}, sizeof(double) * 3);
@@ -125,13 +130,63 @@ void loop()
     printf("stopped");
 }
 
+int updateCounter = 0;
 void update()
 {
+    // update_rotation_matrix_x(XRotationMatrix, 0);
+    // update_rotation_matrix_y(YRotationMatrix, 0);
+    // update_rotation_matrix_z(ZRotationMatrix, 0);
+
+    for (int i = 0; i < num_torus_points; i++)
+    {
+        gsl_vector *original = gsl_vector_alloc(3);
+        gsl_vector_memcpy(original, toruspoints[i]); // original <-- toruspoints[i]
+        gsl_blas_dgemv(CblasNoTrans,                 // no transpose
+                       1.0,                          // alpha=1
+                       XRotationMatrix,              // matrix
+                       original,                     // vector
+                       0.0,                          // beta,
+                       toruspoints[i]);              // result
+
+        gsl_vector_memcpy(original, toruspoints[i]); // original <-- toruspoints[i]
+        gsl_blas_dgemv(CblasNoTrans,                 // no transpose
+                       1.0,                          // alpha=1
+                       YRotationMatrix,              // matrix
+                       original,                     // vector
+                       0.0,                          // beta,
+                       toruspoints[i]);              // result
+
+        gsl_vector_memcpy(original, toruspoints[i]); // original <-- toruspoints[i]
+        gsl_blas_dgemv(CblasNoTrans,                 // no transpose
+                       1.0,                          // alpha=1
+                       ZRotationMatrix,              // matrix
+                       original,                     // vector
+                       0.0,                          // beta,
+                       toruspoints[i]);              // result
+    }
+
+    updateCounter++;
 }
 
 void display()
 {
+    // clearing the actual console
     system("clear");
+
+    // clearing the screen characters array
+    for (int y = 0, x; y < h; y++)
+        for (x = 0; x < w; x++)
+            screen[x][y] = ' ';
+
+    // transfer the torus pixels onto the screen array
+    // NOTE: We assume the middle of the screen is the origin
+    for (int i = 0, x, y; i < num_torus_points; i++)
+    {
+        x = (gsl_vector_get(toruspoints[i], 0)) + w / 2;
+        y = (gsl_vector_get(toruspoints[i], 1)) + h / 2;
+        if (x >= 0 && y >= 0 && x < w && y < h)
+            screen[x][y] = '#';
+    }
 
     for (int y = 0, x; y < h; printf("\n"), y++)
         for (x = 0; x < w; x++)
@@ -145,6 +200,9 @@ void cleanup()
     for (int i = 0; i < num_torus_points; gsl_vector_free(toruspoints[i]), i++)
         ;
 
+    gsl_matrix_free(YRotationMatrix);
+    gsl_matrix_free(XRotationMatrix);
+    gsl_matrix_free(ZRotationMatrix);
     // gsl_vector_free(torus_mid_point);
 }
 
@@ -178,4 +236,36 @@ void update_rotation_matrix_y(gsl_matrix *rot_mat, double angle)
     gsl_matrix_set(rot_mat, 2, 0, -s);
     gsl_matrix_set(rot_mat, 2, 1, 0.0);
     gsl_matrix_set(rot_mat, 2, 2, c);
+}
+
+void update_rotation_matrix_z(gsl_matrix *rot_matrix, double angle)
+{
+    double c = cos(angle);
+    double s = sin(angle);
+
+    gsl_matrix_set(rot_matrix, 0, 0, c);
+    gsl_matrix_set(rot_matrix, 0, 1, -s);
+    gsl_matrix_set(rot_matrix, 0, 2, 0.0);
+    gsl_matrix_set(rot_matrix, 1, 0, s);
+    gsl_matrix_set(rot_matrix, 1, 1, c);
+    gsl_matrix_set(rot_matrix, 1, 2, 0.0);
+    gsl_matrix_set(rot_matrix, 2, 0, 0.0);
+    gsl_matrix_set(rot_matrix, 2, 1, 0.0);
+    gsl_matrix_set(rot_matrix, 2, 2, 1.0);
+}
+
+void update_rotation_matrix_x(gsl_matrix *rot_matrix, double angle)
+{
+    double c = cos(angle);
+    double s = sin(angle);
+
+    gsl_matrix_set(rot_matrix, 0, 0, 1.0);
+    gsl_matrix_set(rot_matrix, 0, 1, 0.0);
+    gsl_matrix_set(rot_matrix, 0, 2, 0.0);
+    gsl_matrix_set(rot_matrix, 1, 0, 0.0);
+    gsl_matrix_set(rot_matrix, 1, 1, c);
+    gsl_matrix_set(rot_matrix, 1, 2, -s);
+    gsl_matrix_set(rot_matrix, 2, 0, 0.0);
+    gsl_matrix_set(rot_matrix, 2, 1, s);
+    gsl_matrix_set(rot_matrix, 2, 2, c);
 }
